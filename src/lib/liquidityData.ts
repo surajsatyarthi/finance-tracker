63bf66d4733e8c3fa73995db004205ff7a5bdc7f
surajsatyarthi <kriger.5490@gmail.com>
2025-12-07 20:24:00 +0530

    feat(accounts): add data initialization button and update default balances

--- src/lib/liquidityData.ts
+++ src/lib/liquidityData.ts
@@ -1,6 +1,6 @@
 
 export const initialLiquidity = [
-    { name: 'Cash', type: 'cash', balance: 55.00, currency: 'INR' },
+    { name: 'Cash', type: 'cash', balance: 1005.00, currency: 'INR' },
     { name: 'SBI', type: 'savings', balance: 122552.00, currency: 'INR' },
     { name: 'CBI', type: 'savings', balance: 901.00, currency: 'INR' },
     { name: 'Jupiter', type: 'digital_bank', balance: 31150.00, currency: 'INR' },
